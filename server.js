8c0bb56ef9e27b13c1fd13606b7094f19e3c6f16
I339171 <rohith.m.k@sap.com>
2018-08-22 15:06:35 +0530

    Commiting changes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('*', function(req, res){
     // const index = path.join(__dirname, 'dist','index.html');
-    res.sendFile('/dist/index.html');
+    res.sendFile('./dist/index.html');
 });
 
 console.log("Welcome To my page!!");
