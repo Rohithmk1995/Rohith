6a38ee98993a846fc5e4874b53b2012ab1da1816
I339171 <rohith.m.k@sap.com>
2018-08-22 14:54:52 +0530

    Commiting changes

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function(req, res){
+    const index = path.join(__dirname, 'dist','index.html');
+    res.sendFile(index);
 });
 
 console.log("Welcome To my page!!");
